6ddc888b7bdc63dd6c4ae6c5abc599dc4a1bf367
Victor <victor.gil.aitaca@gmail.com>
2023-03-30 16:36:55 +0200

    Test_I_pass

--- NumerosRomanos.py
+++ NumerosRomanos.py
@@ -1,3 +1,10 @@
 
 class NumerosRomanos:
-    pass
+    def __init__(self):
+        self.resultado = 0
+
+    def numero(self, num_romano):
+        for x in num_romano:
+            if len(num_romano) == 1:
+                if num_romano == "I":
+                    return 1
